13c71909fdcca23d6143f9801a51fce5f18cd12f
yogi <4215078yogi@gmail.com>
2022-02-02 08:26:26 +0900

    2nd commit

--- main copy.py
+++ main copy.py
@@ -4,12 +4,13 @@ import io
 import requests
 from PIL import Image
 from PIL import ImageDraw
+from PIL import ImageFont
 
 st.title('顔認識アプリ')
 
 subscription_key ='SUBSCRIPTION_KEY'
 assert subscription_key
-face_api_url = 'FACE_API_URL'
+face_api_url = 'ENDPOINT/face/v1.0/detect'
 
 uploaded_file = st.file_uploader("choose an image...", type='jpeg')
 
@@ -35,8 +36,19 @@ if uploaded_file is not None:
   results = res.json()
 
   for result in results:
+    def get_text_rectangle():
+      left = (rect['left'] + (rect['left'] + rect['width'])) / 2
+      top = rect['top'] -20
+      return (left, top)
+      
     rect =result['faceRectangle']
+    txpos = get_text_rectangle()
+    age = str(result['faceAttributes']['age'])
+    textsize = 14
+    font = ImageFont.truetype("/Library/Fonts/Arial Bold.ttf", size=textsize)
+    textcolor = (255, 255, 255)
+
     draw = ImageDraw.Draw(img)
     draw.rectangle([(rect['left'],rect['top']),(rect['left']+rect['width'], rect['top']+rect['height'])], fill=None, outline='green', width=3)
-
+    draw.text(txpos, age, font=font, fill=textcolor)
   st.image(img, caption='Uploaded Image.', use_column_width=True)
